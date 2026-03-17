43ec84ece043fa7dd8ce762e42cef5ca516c924d
Joseph Yamoah <louiscj0001@gmail.com>
2026-03-17 09:05:11 +0000

    Added Student and Task models

---
+++ lib/models/student.dart
@@ -0,0 +1,13 @@
+class Student {
+  final String name;
+  final String studentId;
+  final String programName; // using "programName" as per spec
+  final int level;
+
+  Student({
+    required this.name,
+    required this.studentId,
+    required this.programName,
+    required this.level,
+  });
+}
